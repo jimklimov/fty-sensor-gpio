04f2cc11d2663d15fae3fe5f1877ca9f953c5f2d
Arnaud Quette <ArnaudQuette@Eaton.com>
2017-05-04 12:42:43 +0200

    Problem: Missing defines Solution: Add these
    Signed-off-by: Arnaud Quette <ArnaudQuette@Eaton.com>

--- include/libgpio.h
+++ include/libgpio.h
@@ -22,6 +22,25 @@
 #ifndef LIBGPIO_H_INCLUDED
 #define LIBGPIO_H_INCLUDED
 
+// Target at gpiochip488
+#define GPIO_BASE_INDEX    488
+
+// Directions
+#define GPIO_DIRECTION_IN    0
+#define GPIO_DIRECTION_OUT   1
+
+// GPIO Status value
+#define GPIO_STATUS_UNKNOWN -1
+#define GPIO_STATUS_CLOSED   0
+#define GPIO_STATUS_OPENED   1
+
+// Defines
+#define GPIO_BUFFER_MAX      4
+#define GPIO_DIRECTION_MAX  35
+#define GPIO_VALUE_MAX      30
+#define GPI_MAX_NUM         10
+
+
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -30,6 +49,21 @@ extern "C" {
 //  Create a new libgpio
 FTY_SENSOR_GPIO_EXPORT libgpio_t *
     libgpio_new (void);
+
+//  @interface
+//  Read a GPI or GPO status
+FTY_SENSOR_GPIO_EXPORT int
+    libgpio_read (libgpio_t **self_p, int pin);
+
+//  @interface
+//  Write a GPO (to enable or disable it)
+FTY_SENSOR_GPIO_EXPORT int
+    libgpio_write (libgpio_t **self_p, int pin, int value);
+
+//  @interface
+//  Get the textual name for a status
+FTY_SENSOR_GPIO_EXPORT string
+    libgpio_get_status_string (libgpio_t **self_p, int value);
 
 //  Destroy the libgpio
 FTY_SENSOR_GPIO_EXPORT void
