6141b894b7d41e551b9ba93d6a96aef909ba3534
Arnaud Quette <ArnaudQuette@Eaton.com>
2017-05-30 14:20:57 +0200

    Problem: Function signature not up to date Solution: Update it
    Signed-off-by: Arnaud Quette <ArnaudQuette@Eaton.com>

--- include/fty_sensor_gpio.h
+++ include/fty_sensor_gpio.h
@@ -64,7 +64,9 @@ const char* s_get (zconfig_t *config, const char* key, const char*dfl);
 
 // Implemented in assets actor
 extern zlistx_t *_gpx_list;
-extern zlistx_t * get_gpx_list();
+extern zlistx_t * get_gpx_list(bool verbose);
 extern pthread_mutex_t gpx_list_mutex;
+
+#define my_zsys_debug(verbose, ...) { if (verbose) zsys_debug (__VA_ARGS__); }
 
 #endif
