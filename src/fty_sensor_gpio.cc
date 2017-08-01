fa18e86d167384f8b8026864e084ea34c5cd9c1e
Jana Rapava <janarapava@eaton.com>
2017-08-01 16:48:24 +0200

    Problem: We want to make configuration consistent with fty-info.
    Solution: Use check_interval for how often to check sensor state.
    
    Signed-off-by: Jana Rapava <janarapava@eaton.com>

--- src/fty_sensor_gpio.cc
+++ src/fty_sensor_gpio.cc
@@ -1,21 +1,21 @@
 /*  =========================================================================
     fty_sensor_gpio - Manage GPI sensors and GPO devices
 
-    Copyright (C) 2014 - 2017 Eaton                                        
-                                                                           
-    This program is free software; you can redistribute it and/or modify   
-    it under the terms of the GNU General Public License as published by   
-    the Free Software Foundation; either version 2 of the License, or      
-    (at your option) any later version.                                    
-                                                                           
-    This program is distributed in the hope that it will be useful,        
-    but WITHOUT ANY WARRANTY; without even the implied warranty of         
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          
-    GNU General Public License for more details.                           
-                                                                           
+    Copyright (C) 2014 - 2017 Eaton
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
     You should have received a copy of the GNU General Public License along
     with this program; if not, write to the Free Software Foundation, Inc.,
-    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.            
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
     =========================================================================
 */
 
@@ -43,7 +43,7 @@
 //   gpi_mapping
 //       <gpi number> = <pin number>
 // To be discussed:
-// * Check for convergence with other dry-contacts (on EMP001 and fty-sensor-env, EMP002, 
+// * Check for convergence with other dry-contacts (on EMP001 and fty-sensor-env, EMP002,
 // * Add 'int last_state' to '_gpx_info_t' and only publish state change?
 //   (i.e. last_state != current_state)
 // * i18n for alerts and $status
@@ -125,7 +125,7 @@ int main (int argc, char *argv [])
             verbose = true;
         }
         // Polling interval
-        str_poll_interval = s_get (config, "server/poll_interval", "2000");
+        str_poll_interval = s_get (config, "server/check_interval", "2000");
         if (str_poll_interval) {
             poll_interval = atoi(str_poll_interval);
         }
