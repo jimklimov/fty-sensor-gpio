8e064921d76dda67a622ceb7ba16fddacbabfe76
Jim Klimov <jim@jimklimov.com>
2020-04-23 12:00:39 +0200

    Bump copyrights to 2020

--- src/fty_sensor_gpio_assets.cc
+++ src/fty_sensor_gpio_assets.cc
@@ -1,7 +1,7 @@
 /*  =========================================================================
     fty_sensor_gpio_assets - 42ITy GPIO assets handler
 
-    Copyright (C) 2014 - 2017 Eaton
+    Copyright (C) 2014 - 2020 Eaton
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
